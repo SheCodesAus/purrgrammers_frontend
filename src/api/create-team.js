84bcb5e545a01e10955dc8907a197706ec31d329
Emma Spear <emmspear@gmail.com>
2025-11-26 13:02:19 +0800

    feat: created api call for creating a team

---
+++ src/api/create-team.js
@@ -0,0 +1,27 @@
+async function createTeam(teamData, token) {
+    const url = `${import.meta.env.VITE_API_URL}/api/teams/`;
+
+    const response = await fetch(url , {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": token
+        },
+        body: JSON.stringify(teamData)
+    });
+
+    if (!response.ok) {
+        const fallbackError = "Error creating team";
+
+        const data = await response.json().catch(() => {
+            throw new Error(fallbackError)
+        });
+
+        const errorMessage = data?.detail ?? fallbackError;
+        throw new Error(errorMessage)
+    }
+
+    return await response.json();
+};
+
+export default createTeam;
