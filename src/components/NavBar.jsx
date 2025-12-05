03897ff3dee4b0925729fe75938d7908d7bdc684
Emma Spear <emmspear@gmail.com>
2025-12-05 13:59:40 +0800

    css for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from 'react-router-dom';
 import { useAuth } from '../hooks/use-auth';
 import './NavBar.css';
+import logo from '../assets/logo-2.svg';
 
 function NavBar() {
     const { auth, setAuth } = useAuth();
@@ -15,22 +16,28 @@ function NavBar() {
     return (
         <nav className='nav-container'> {/* semantic HTML for accessibility and SEO benefits*/}    
             <div className='nav-bar'>
+                <div className='nav-bar-left'>
+                    
+                    <Link to="/" className='nav-logo'>
+                        <img src={logo} alt="Save Point" className="nav-logo-img" />
+                    </Link>   
+                </div>
                 <ul className='nav-links'>
-                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/" className={auth?.token ? 'auth-link' : ''}>Home</NavLink></li>
 
                     {/* Show login only when not logged in */}
                     {!auth?.token && (
-                        <li><NavLink to="/login">Login</NavLink></li>  
+                        <li><NavLink to="/login" className="login-btn">Login</NavLink></li>  
                     )}
                     
                     {/* Show Dashboard only when logged in */}
                     {auth?.token && (
-                        <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+                        <li><NavLink to="/dashboard" className="auth-link">Dashboard</NavLink></li>
                     )}
                     
                     {/* Show Logout only when logged in */}
                     {auth?.token && (
-                        <li><button onClick={handleLogout} className="logout-btn">Logout</button></li>
+                        <li><button onClick={handleLogout} className="logout-btn auth-link">Logout</button></li>
                     )}
                 </ul>
             </div>
